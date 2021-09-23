a555f7bd463f718b650c4c8ecc4457bf3207ec28
Natan Colleoni <natancolleonifonseca@gmail.com>
2021-09-23 19:02:11 -0300

    Creating project

---
+++ CRUDmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
