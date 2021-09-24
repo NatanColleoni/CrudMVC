c85360fe0e6f5dc866b66626ab323546e0ca76a3
Natan Colleoni <natancolleonifonseca@gmail.com>
2021-09-24 16:26:04 -0300

    Scaffold for department created

---
+++ CRUDmvc/Data/CRUDmvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDmvc.Models
+{
+    public class CRUDmvcContext : DbContext
+    {
+        public CRUDmvcContext (DbContextOptions<CRUDmvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUDmvc.Models.Department> Department { get; set; }
+    }
+}
--- CRUDmvc/Startup.cs
+++ CRUDmvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUDmvc.Models;
 
 namespace CRUDmvc
 {
@@ -33,6 +35,9 @@ namespace CRUDmvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CRUDmvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CRUDmvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
