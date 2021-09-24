9db8b408be6bdca31be375e54244f5e09fdc7def
Natan Colleoni <natancolleonifonseca@gmail.com>
2021-09-24 17:01:22 -0300

    Adapting project for MySQL and creating Departments Migration

--- CRUDmvc/Startup.cs
+++ CRUDmvc/Startup.cs
@@ -37,7 +37,11 @@ namespace CRUDmvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<CRUDmvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CRUDmvcContext")));
+                    options.UseMySql(
+                        Configuration.GetConnectionString("CRUDmvcContext"),
+                        builder => builder.MigrationsAssembly("CRUDmvc")
+                        )
+                    );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
